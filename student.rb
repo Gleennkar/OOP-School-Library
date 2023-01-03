54ad5d2872803044dafa37b6fd8bb0361865a679
Odette Gleen <nkargleeny@gmail.com>
2023-01-03 19:37:10 +0100

    fix linters

--- student.rb
+++ student.rb
@@ -2,10 +2,9 @@ require_relative 'person'
 
 Class Student < Person
 def initialize (age, classroom, name = "Unknown", parent_permission = true)
-    super (age, name, parent_permission)
+    super ( age, name, parent_permission )
     @classroom = classroom
 end
-
 
  def play_hooky
     '¯\(ツ)/¯'
--- teacher.rb
+++ teacher.rb
@@ -3,7 +3,7 @@ require_relative 'person'
 class Teacher < person
   
         def initialize (age, name = "Unknown", parent_pemission = true, specialization)
-            super (age, name, parent_pemission)
+            super ( age, name, parent_pemission )
             @specialization = specialization
         end
 
