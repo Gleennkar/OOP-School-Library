c9c84585d2ba3f9e1264eddde554841c1d99a96d
Odette Gleen <nkargleeny@gmail.com>
2023-01-03 21:29:49 +0100

    carried changes

--- person.rb
+++ person.rb
@@ -10,7 +10,7 @@ class Person
   end
 
   def can_use_service?
-    is_of_age || @parent_permission
+    of_age || @parent_permission
   end
 
   private
--- teacher.rb
+++ teacher.rb
@@ -3,7 +3,7 @@ require_relative 'person'
 class Teacher < person
   def initialize(_specilization, age, name = 'Unknown', parent_pemission: true)
     super age, name, parent_pemission
-    @specialization = specialization
+    @specialization = _specialization
   end
 
   def can_use_service?
